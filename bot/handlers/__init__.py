789ff55df5ae632e81b8497b0dab82c283c98ee7
limbo2341 <igorgrabovetc21@gmail.com>
2026-05-14 12:46:46 +0300

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,11 +1,13 @@
 from aiogram import Router
 
 from .start import router as start_router
+from .search import router as search_router
 
 
 def setup_routers() -> Router:
     router = Router()
 
     router.include_router(start_router)
+    router.include_router(search_router)
 
     return router
