c0d9a8385ba5e6501d1bfa4de390a5d49c31c715
limbo2341 <igorgrabovetc21@gmail.com>
2026-05-14 11:39:41 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,17 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
+router = Router()
+
+
+@router.message(CommandStart())
+async def start_command(message: Message):
+    await message.answer(
+        "🎮 <b>Искатель Игры Детства</b>\n\n"
+        "Я помогу найти игру по памяти.\n\n"
+        "📝 Опиши игру\n"
+        "📷 Или отправь скриншот\n\n"
+        "Пример:\n"
+        "<i>игра где пустыня и машины</i>"
+    )
